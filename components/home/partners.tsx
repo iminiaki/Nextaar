58c188f23ec92c9dbb359fe5511439a9eab04acc
iminiaki <iminiaki@gmail.com>
2025-09-03 22:17:01 +0330

    refactor: update partner logos and enhance styling
    - Replaced existing partner logos with a new set of technology logos for better relevance.
    - Improved the styling of the partner logo section by adding a hover effect and margin adjustments for better visual appeal.
    - Removed outdated logo files from the public directory to clean up assets.

--- components/home/partners.tsx
+++ components/home/partners.tsx
@@ -2,18 +2,36 @@ import { RevealOnScroll } from "@/components/gsap/reveal"
 
 export function Partners({ title }: { title: string }) {
   const logos = [
-    "/generic-brand-logo-1.png",
-    "/generic-brand-logo-2.png",
-    "/brand-logo-3.png",
-    "/brand-logo-4.png",
-    "/brand-logo-5.png",
-    "/brand-logo-6.png",
+    "/techs/Backbone.js.svg",
+    "/techs/Bootstrap.svg",
+    "/techs/Express.svg",
+    "/techs/Firebase.svg",
+    "/techs/Gatsby.svg",
+    "/techs/GraphQL.svg",
+    "/techs/Laravel.svg",
+    "/techs/Material UI.svg",
+    "/techs/MongoDB.svg",
+    "/techs/MySQL.svg",
+    "/techs/Nest.js.svg",
+    "/techs/Next.js.svg",
+    "/techs/NGINX.svg",
+    "/techs/Node.js.svg",
+    "/techs/Nuxt JS.svg",
+    "/techs/payload.svg",
+    "/techs/PHP.svg",
+    "/techs/PostgresSQL.svg",
+    "/techs/React.svg",
+    "/techs/Solid.js.svg",
+    "/techs/Tailwind CSS.svg",
+    "/techs/TypeScript.svg",
+    "/techs/Vite.js.svg",
+    "/techs/Vue.js.svg",
   ]
   return (
     <section className="py-12">
       <div className="container mx-auto px-4">
         <div className="mx-auto max-w-2xl text-center">
-          <h3 className="text-xl font-medium">{title}</h3>
+          <h3 className="text-xl font-medium mb-12">{title}</h3>
         </div>
         <RevealOnScroll staggerChildren className="mt-6">
           <div className="grid grid-cols-2 items-center justify-center gap-6 sm:grid-cols-3 md:grid-cols-6">
@@ -22,7 +40,7 @@ export function Partners({ title }: { title: string }) {
                 <img
                   src={src || "/placeholder.svg"}
                   alt={"Partner logo " + (i + 1)}
-                  className="h-10 opacity-70 grayscale"
+                  className="h-10 grayscale hover:grayscale-0 transition-grayscale duration-300"
                 />
               </div>
             ))}
