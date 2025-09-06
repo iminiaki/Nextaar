382728bd47d7ea085be068492417731f24fe7fd7
iminiaki <iminiaki@gmail.com>
2025-09-06 10:43:46 +0330

    feat: adjust grid layout in BlogPage for improved post display
    - Modified the grid layout in the BlogPage component to support 4 columns on larger screens, enhancing the visual presentation of posts.
    - This change aligns with recent updates to increase content visibility and improve user experience.

--- app/(app)/[locale]/blog/page.tsx
+++ app/(app)/[locale]/blog/page.tsx
@@ -39,7 +39,7 @@ export default async function BlogPage({
       </RevealOnScroll>
 
       <RevealOnScroll staggerChildren className="mt-10">
-        <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
           {posts
             .sort((a, b) => (a.createdAt < b.createdAt ? 1 : -1))
             .map((p) => (
